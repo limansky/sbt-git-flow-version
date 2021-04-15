b0108dc706127398f74c81c29a827b447ce20a5c
Mike Limansky <mike.limansky@gmail.com>
2021-04-15 18:56:45 +0300

    Fix tests

--- src/test/scala/sbtgitflowversion/VersionCalculatorTest.scala
+++ src/test/scala/sbtgitflowversion/VersionCalculatorTest.scala
@@ -8,64 +8,79 @@ class VersionCalculatorTest extends AnyFlatSpec with Matchers {
 
   val prev = VersionNumber("1.2.3")
   val cur = VersionNumber("1.3.0")
+  val max = VersionNumber("1.4.0")
 
   "VersionCalculator" should "support last version policy" in {
-    VersionCalculator.lastVersion()(prev, Some(cur), Some("foo")) shouldEqual Right(prev)
-    VersionCalculator.lastVersion()(prev, None, Some("foo")) shouldEqual Right(prev)
-    VersionCalculator.lastVersion()(prev, Some(cur), None) shouldEqual Right(prev)
-    VersionCalculator.lastVersion()(prev, None, None) shouldEqual Right(prev)
+    VersionCalculator.lastVersion()(prev, Some(cur), max, Some("foo")) shouldEqual Right(prev)
+    VersionCalculator.lastVersion()(prev, None, max, Some("foo")) shouldEqual Right(prev)
+    VersionCalculator.lastVersion()(prev, Some(cur), max, None) shouldEqual Right(prev)
+    VersionCalculator.lastVersion()(prev, None, max, None) shouldEqual Right(prev)
   }
 
   it should "support last version with suffix policy" in {
-    VersionCalculator.lastVersionWithSuffix("epic")(prev, Some(cur), Some("foo")) shouldEqual Right(
+    VersionCalculator.lastVersionWithSuffix("epic")(prev, Some(cur), max, Some("foo")) shouldEqual Right(
       VersionNumber("1.2.3-epic-SNAPSHOT")
     )
 
-    VersionCalculator.lastVersionWithSuffix("epic", isSnapshot = false)(prev, None, Some("foo")) shouldEqual Right(
+    VersionCalculator.lastVersionWithSuffix("epic", isSnapshot = false)(prev, None, max, Some("foo")) shouldEqual Right(
       VersionNumber("1.2.3-epic")
     )
 
-    VersionCalculator.lastVersionWithSuffix("suffix")(prev, Some(cur), None) shouldEqual Right(
+    VersionCalculator.lastVersionWithSuffix("suffix")(prev, Some(cur), max, None) shouldEqual Right(
       VersionNumber("1.2.3-suffix-SNAPSHOT")
     )
-    VersionCalculator.lastVersionWithSuffix("test", isSnapshot = false)(prev, None, None) shouldEqual Right(
+    VersionCalculator.lastVersionWithSuffix("test", isSnapshot = false)(prev, None, max, None) shouldEqual Right(
       VersionNumber("1.2.3-test")
     )
   }
 
   it should "support version with matching" in {
-    VersionCalculator.lastVersionWithMatching()(prev, Some(cur), Some("foo")) shouldEqual Right(
+    VersionCalculator.lastVersionWithMatching()(prev, Some(cur), max, Some("foo")) shouldEqual Right(
       VersionNumber("1.2.3-foo-SNAPSHOT")
     )
-    VersionCalculator.lastVersionWithMatching()(prev, None, Some("foo")) shouldEqual Right(
+    VersionCalculator.lastVersionWithMatching()(prev, None, max, Some("foo")) shouldEqual Right(
       VersionNumber("1.2.3-foo-SNAPSHOT")
     )
-    VersionCalculator.lastVersionWithMatching()(prev, Some(cur), None) shouldEqual Left(
+    VersionCalculator.lastVersionWithMatching()(prev, Some(cur), max, None) shouldEqual Left(
       "Empty matching is not allowed for lastVersionWithMatching policy"
     )
   }
 
   it should "support matching policy" in {
-    VersionCalculator.matching()(prev, Some(cur), Some("2.3.4")) shouldEqual Right(VersionNumber("2.3.4-SNAPSHOT"))
-    VersionCalculator.matching(isSnapshot = false)(prev, None, Some("2.3.4")) shouldEqual Right(VersionNumber("2.3.4"))
-    VersionCalculator.matching()(prev, Some(cur), None) shouldEqual Left(
+    VersionCalculator.matching()(prev, Some(cur), max, Some("2.3.4")) shouldEqual Right(VersionNumber("2.3.4-SNAPSHOT"))
+    VersionCalculator.matching(isSnapshot = false)(prev, None, max, Some("2.3.4")) shouldEqual Right(
+      VersionNumber("2.3.4")
+    )
+    VersionCalculator.matching()(prev, Some(cur), max, None) shouldEqual Left(
       "Empty matching is not allowed for matching policy"
     )
-    VersionCalculator.matching()(prev, None, Some("abc")) shouldEqual Left(
+    VersionCalculator.matching()(prev, None, max, Some("abc")) shouldEqual Left(
       "Empty matching is not allowed for matching policy"
     )
   }
 
   it should "support current tag policy" in {
-    VersionCalculator.currentTag()(prev, Some(cur), Some("foo")) shouldEqual Right(cur)
-    VersionCalculator.currentTag()(prev, Some(cur), None) shouldEqual Right(cur)
-    VersionCalculator.currentTag()(prev, None, Some("foo")) shouldEqual Left("No tag defined for current version")
+    VersionCalculator.currentTag()(prev, Some(cur), max, Some("foo")) shouldEqual Right(cur)
+    VersionCalculator.currentTag()(prev, Some(cur), max, None) shouldEqual Right(cur)
+    VersionCalculator.currentTag()(prev, None, max, Some("foo")) shouldEqual Left("No tag defined for current version")
   }
 
   it should "support nextX policies" in {
-    VersionCalculator.nextBuild()(prev, Some(cur), Some("foo")) shouldEqual Right(VersionNumber("1.2.4-SNAPSHOT"))
-    VersionCalculator.nextMinor()(prev, Some(cur), Some("foo")) shouldEqual Right(VersionNumber("1.3.0-SNAPSHOT"))
-    VersionCalculator.nextMajor(isSnapshot = false)(prev, Some(cur), Some("foo")) shouldEqual Right(
+    VersionCalculator.nextBuild()(prev, Some(cur), max, Some("foo")) shouldEqual Right(VersionNumber("1.2.4-SNAPSHOT"))
+    VersionCalculator.nextMinor()(prev, Some(cur), max, Some("foo")) shouldEqual Right(VersionNumber("1.3.0-SNAPSHOT"))
+    VersionCalculator.nextMajor(isSnapshot = false)(prev, Some(cur), max, Some("foo")) shouldEqual Right(
+      VersionNumber("2.0.0")
+    )
+  }
+
+  it should "support globalNextX policies" in {
+    VersionCalculator.globalNextBuild()(prev, Some(cur), max, Some("foo")) shouldEqual Right(
+      VersionNumber("1.4.1-SNAPSHOT")
+    )
+    VersionCalculator.globalNextMinor()(prev, Some(cur), max, Some("foo")) shouldEqual Right(
+      VersionNumber("1.5.0-SNAPSHOT")
+    )
+    VersionCalculator.globalNextMajor(isSnapshot = false)(prev, Some(cur), max, Some("foo")) shouldEqual Right(
       VersionNumber("2.0.0")
     )
   }
